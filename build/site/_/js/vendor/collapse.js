49debd1eb50369f70f415ff632e6ca954cfb5a76
Alex Mansell <alex.mansell@magnolia-cms.com>
2021-08-24 10:50:12 +0100

    rebuild for topher

---
+++ build/site/_/js/vendor/collapse.js
@@ -0,0 +1,1 @@
+"use strict";for(var coll=document.getElementsByClassName("collapse"),i=0;i<coll.length;i++)coll[i].addEventListener("click",function(){this.classList.toggle("active");var l=this.nextElementSibling;"block"===l.style.display?l.style.display="none":l.style.display="block"});
